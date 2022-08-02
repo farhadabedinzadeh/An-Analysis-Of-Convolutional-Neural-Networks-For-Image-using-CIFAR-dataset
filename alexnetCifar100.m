62d49585d662dc14f9f1c70701e40e71173dc035
Farhad Abedinzadeh Torghabeh <96732467+farhadabedinzadeh@users.noreply.github.com>
2022-08-02 23:39:41 +0430

    Add files via upload

---
+++ alexnetCifar100.m
@@ -0,0 +1,83 @@
+clc;clear;close
+%% '================ Written by Farhad AbedinZadeh ================'
+%                                                                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('train.mat')
+alldata=data;
+alllabel=labels;
+
+load('test.mat')
+testdata=data;
+testlabel=labels;
+
+
+net = alexnet;
+analyzeNetwork(net)
+net.Layers(1)
+inputSize = net.Layers(1).InputSize;
+if isa(net,'SeriesNetwork') 
+  lgraph = layerGraph(net.Layers); 
+else
+  lgraph = layerGraph(net);
+end 
+
+[learnableLayer,classLayer] = findLayersToReplace(lgraph);
+[learnableLayer,classLayer] 
+
+numClasses = numel(categories(alllabel));
+
+if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
+    newLearnableLayer = fullyConnectedLayer(numClasses, ...
+        'Name','new_fc', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+    
+elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
+    newLearnableLayer = convolution2dLayer(1,numClasses, ...
+        'Name','new_conv', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+end
+
+lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+
+newClassLayer = classificationLayer('Name','new_classoutput');
+lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
+
+figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
+plot(lgraph)
+ylim([0,10])
+
+layers = lgraph.Layers;
+connections = lgraph.Connections;
+
+layers(1:10) = freezeWeights(layers(1:10));
+lgraph = createLgraphUsingConnections(layers,connections);
+
+pixelRange = [-30 30];
+scaleRange = [0.9 1.1];
+imageAugmenter = imageDataAugmenter( ...
+    'RandXReflection',true, ...
+    'RandXTranslation',pixelRange, ...
+    'RandYTranslation',pixelRange, ...
+    'RandXScale',scaleRange, ...
+    'RandYScale',scaleRange);
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),alldata, ...
+    'DataAugmentation',imageAugmenter);
+
+augimdsValidation = augmentedImageDatastore(inputSize(1:2),testdata);
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',10, ...
+    'MaxEpochs',6, ...
+    'InitialLearnRate',3e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',augimdsValidation, ...
+    'ValidationFrequency',3, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(augimdsTrain,lgraph,options);
+
+[YPred,probs] = classify(net,augimdsValidation);
+accuracy = mean(YPred == testlabel)
